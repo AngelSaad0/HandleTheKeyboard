19c3b81cefb3e68f7a86ba7c2dcad016375eaa5c
AngelSaad0 <engysaad573new@gmail.com>
2024-11-29 23:16:53 +0200

    setup Notification on keyboard Appear

--- How To Handle The Keyboard/ViewController.swift
+++ How To Handle The Keyboard/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textLabel: UITextField!
+    @IBOutlet var buttonConstrains: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNotificationCenter()
+        keyboardHidding()
+    }
+    private func setupNotificationCenter(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisplay), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillDisplay(notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRect = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRect.height
+            buttonConstrains.constant = keyboardHeight - 30
+        }
+    }
+
+    @objc func keyboardHide(){
+        buttonConstrains.constant = 50
+    }
+
+
+
+    func keyboardHidding() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(keyboardRemove))
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    @objc func keyboardRemove(){
+        view.endEditing(true)
     }
 
 
